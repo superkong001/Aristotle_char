2aa7e9df548b939f6084b6efb4b485729dc4d8c0
telos <37318654+superkong001@users.noreply.github.com>
2024-03-12 16:03:37 +0800

    Update solomon_Webchart.py

--- solomon_Webchart.py
+++ solomon_Webchart.py
@@ -188,33 +188,10 @@ def load_model():
         .to(torch.bfloat16)
         .cuda()
     )
-    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-    
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)    
     model_path = snapshot_download(model_id, revision='master')
-
-    # 定义模型路径(xlab)
-    # from openxlab.model import download
-    # from transformers import AutoTokenizer, AutoModelForCausalLM
-    # model_id = 'telos/solomon_chart'
-    # model_name = 'solomon_chart'
-    # # model_path = '/home/xlab-app-center/.cache' # '/home/xlab-app-center/.cache/model'
-    # model_path = './'
-    # download(model_repo=model_id, model_name=model_name)
-    
-    # # 从预训练的模型中获取模型，并设置模型参数
-    # model = (AutoModelForCausalLM.from_pretrained(model_path,
-    #                                               trust_remote_code=True).to(
-    #                                                   torch.bfloat16).cuda())
-    
-    # # 从预训练的模型中获取tokenizer
-    # tokenizer = AutoTokenizer.from_pretrained(model_path,
-    #                                           trust_remote_code=True)
-
-    
-    # model.eval()  
     
     return model, tokenizer, model_path
-
 
 def prepare_generation_config():
     with st.sidebar:
